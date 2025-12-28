83b5fa5ead04d4966944e11daa0b0e47b1b7208b
jusafaido17 <jusafa.ido@gmail.com>
2025-12-28 12:20:35 +0700

    Fixed middleware

--- app/api/books/[id]/route.js
+++ app/api/books/[id]/route.js
@@ -2,6 +2,49 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
+// GET: Ambil detail satu buku berdasarkan ID (Protected)
+export async function GET(request, { params }) {
+    try {
+        const { id } = await params;
+        const bookId = parseInt(id);
+
+        // 1. Verifikasi Token (Sesuai syarat Poin 4)
+        const authHeader = request.headers.get("authorization");
+        if (!authHeader) {
+            return NextResponse.json({ success: false, error: "Unauthorized", code: 401 }, { status: 401 });
+        }
+
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.JWT_SECRET);
+
+        // 2. Cari data di Database
+        const book = await prisma.book.findUnique({
+            where: { id: bookId }
+        });
+
+        if (!book) {
+            return NextResponse.json({ 
+                success: false, 
+                error: "Unauthorized", 
+                code: 401 
+            }, { status: 401 });
+        }
+
+        return NextResponse.json({ 
+            success: true, 
+            message: "Data buku ditemukan", 
+            data: book 
+        }, { status: 200 });
+
+    } catch (error) {
+        return NextResponse.json({ 
+            success: false, 
+            error: "Unauthorized", 
+            code: 401 
+        }, { status: 401 });
+    }
+}
+
 // DELETE: Menghapus buku (Admin Only)
 export async function DELETE(request, { params }) {
     try {
